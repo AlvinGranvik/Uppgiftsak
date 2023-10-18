c7a3a4a896dfdc4f538d792147fc37ebaa15c517
AlvinGranvik <alvin.granvik@elev.ga.ntig.se>
2023-10-18 09:40:18 +0200

    Initial commit

---
+++ src/Bingles.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Bingles {
+    public static int Points;
+    public static void main(String[] args) {
+        String Betyg;
+        int e = 15;
+        int d = 18;
+        int c = 21;
+        int b = 24;
+        int a = 27;
+
+        if Betyg <=15 then
+            print("YOU HAVE STUPID!!!!");
+
+    }
+}
